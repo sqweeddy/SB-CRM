4986c03eeea923369d989e4cd5ce283d1e7a9c1e
sqweeddy <ex1smor@gamil.com>
2022-04-17 22:39:41 +0700

    fix client create & add select to modal

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,18 @@ import MicroModal from 'micromodal';
 MicroModal.init();
 
 const tableData = document.querySelector('.table__data');
+const saveBtn = document.getElementById('savebtn');
+const nameInput = document.getElementById('name');
+const surnameInput = document.getElementById('surname');
+const lastNameInput = document.getElementById('lastName');
+const modalSelect = document.getElementById('modalSelect');
+const selectInput = document.getElementById('selectInput');
 
 const getData = async () => {
   const response = await fetch('http://localhost:3000/api/clients');
   const result = await response.json();
   return result;
-}
+};
 
 const renderClients = async () => {
   const clients = await getData();
@@ -21,34 +27,48 @@ const renderClients = async () => {
     const clientFio = el('div', client.surname + ' ' + client.name + ' ' + client.lastName);
     const clientCreatedAt = el('div', client.createdAt);
     const clientUpdatedAt = el('div', client.updatedAt);
-    const clientContacts = el('div', client.contacts);
+    const contacts = client.contacts;
+    const clientContacts = el('div');
+    contacts.forEach(contact => {
+      const newContact = el('div', contact.value);
+      setChildren(clientContacts, newContact);
+    })
     const clientChangeBtn = el('button', 'Изменить');
     const clientRemoveBtn = el('button', 'Удалить');
     setChildren(tableData, clientRow);
     setChildren(clientRow, [clientId, clientFio, clientCreatedAt, clientUpdatedAt, clientContacts, clientChangeBtn, clientRemoveBtn])
   });
-}
+};
 
 renderClients();
 
 
-const createClient = (
-  name,
-  surname,
-  lastName,
-  ) => {
+const createClient = () => {
   fetch('http://localhost:3000/api/clients', {
     method: 'POST',
     body: JSON.stringify({
-      name: name,
-      surname: surname,
-      lastName: lastName,
+      name: nameInput.value.trim(),
+      surname: surnameInput.value.trim(),
+      lastName: lastNameInput.value.trim(),
+      contacts: [
+        {
+          type: modalSelect.value,
+          value: selectInput.value,
+        }
+      ]
     }),
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-}
+  });
+  nameInput.value = '';
+  surnameInput.value = '';
+  lastNameInput.value = '';
+  const modalCreate = document.getElementById('modal-1');
+  modalCreate.classList.toggle('is-open');
+  modalCreate.setAttribute('aria-hidden', 'true');
+  renderClients();
+};
 
+saveBtn.addEventListener('click', createClient);
 console.log(getData());
-
